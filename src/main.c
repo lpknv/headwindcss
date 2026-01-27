433b09651002b24117ece332a596076dc4ef2111
lpknv <nikitalipkanov@outlook.com>
2026-01-27 18:48:05 +0100

    add test files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  printf("Hello, HeadwindCSS!");
+  int c;
+
+  FILE *file = fopen("../test/index.html", "r");
+
+  while((c = fgetc(file)) != EOF) {
+    printf("%c",c);
+  }
+
+  fclose(file);
+  fflush(file);
+
   return 0;
 }
