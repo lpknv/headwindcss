3c3dff38b4d7b7709215922b95677d16bced134f
lpknv <nikitalipkanov@outlook.com>
2026-01-30 22:17:08 +0100

    add test file refactor

--- src/headwind.h
+++ src/headwind.h
@@ -25,6 +25,72 @@
 #define AT_SIGN 0x40
 #define EXCLAMATION_MARK 0x21
 
+void css_class_to_property(char *css_class) {
+
+}
+
+void get_css_classes(char *input, const char *css_attribute) {
+  const char *html_class_attribute = css_attribute; // in this case 'class="'
+  int class_end = 0, class_start = 0, c, count_start = 0;
+  char last_char = html_class_attribute[strlen(html_class_attribute) - 1];
+  int token_len = strlen(html_class_attribute), j = 0, css_class_index = 0;
+
+  size_t max_len = 1024;
+  
+  char *css_class_list = malloc(max_len), *buffer;
+
+
+  // Read html file and print its content
+  FILE *file = fopen(input, "rb");
+
+  if(!file) {
+    printf("Error! File not found.");
+    #define FILE_NOT_FOUND
+  }
+
+  // extract css classes and store them
+#ifdef FILE_NOT_FOUND
+  while ((c = fgetc(file)) != EOF) {
+    char character_from_file = (char)c;
+#else
+  for(int i = 0; i < ARRAY_SIZE(input); i++) {
+    char character_from_file = input[i];
+#endif
+
+    if (!count_start) {
+      if (character_from_file == html_class_attribute[j]) {
+        j++;
+        if (j == token_len) {
+          count_start = 1;
+          j = 0;
+        }
+      } else {
+        j = 0;
+      }
+    } else {
+      if (character_from_file == last_char) {
+        count_start = 0;
+        class_end = 1;
+      } else if (css_class_index < max_len - 1) {
+        css_class_list[css_class_index++] = character_from_file;
+      }
+    }
+  }
+
+  css_class_list[css_class_index] = '\0';
+
+  char *css_class_tokens = strtok(css_class_list, " ");
+
+  while(css_class_tokens != NULL) {
+    printf("%s\n", css_class_tokens);
+    css_class_tokens = strtok(NULL, " ");
+  }
+
+  free(css_class_list);
+
+  fclose(file);
+}
+
 /* 
 char* parse(char *input) {
 
--- src/main.c
+++ src/main.c
@@ -1,58 +1,9 @@
 #include "headwind.h"
 
-void extract_css_classes_from_html(char *input);
 
 int main(int argc, char *argv[]) {
 
-  const char *css_class_token = "class=\"";
-  int class_end = 0, class_start = 0, c, count_start = 0;
-  char last_char = css_class_token[strlen(css_class_token) - 1];
-  int token_len = strlen(css_class_token), j = 0, css_class_index = 0;
-
-  size_t max_len = 1024;
-  
-  char *css_class_list = malloc(max_len), *buffer;
-
-
-  // Read html file and print its content
-  FILE *file = fopen("../test/index.html", "rb");
-
-  // extract css classes and store them
-  while ((c = fgetc(file)) != EOF) {
-    char ch = (char)c;
-
-    if (!count_start) {
-      if (ch == css_class_token[j]) {
-        j++;
-        if (j == token_len) {
-          count_start = 1;
-          j = 0;
-        }
-      } else {
-        j = 0;
-      }
-    } else {
-      if (ch == '"') {
-        count_start = 0;
-        class_end = 1;
-      } else if (css_class_index < max_len - 1) {
-        css_class_list[css_class_index++] = ch;
-      }
-    }
-  }
-
-  css_class_list[css_class_index] = '\0';
-
-  char *css_class_tokens = strtok(css_class_list, " ");
-
-  while(css_class_tokens != NULL) {
-    printf("%s\n\n", css_class_tokens);
-    css_class_tokens = strtok(NULL, " ");
-  }
-
-  free(css_class_list);
-
-  fclose(file);
+  get_css_classes("../test/about.html", "class=\"");
 
   return 0;
 }
